b460024575e8ebc85f57bfc777b5abb9536ba171
Nikita Romaneyev <Nikita.Romaneyeb@regium.com>
2023-01-18 02:06:19 +0300

    hypr

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo pacman -S swaybg swayidle grim slurp wl-clipboard dunst xdg-user-dirs rofi foot zsh nerd-fonts-ubuntu starship thunar thunar-volman thunar-archive-plugin gvfs file-roller gsettings-desktop-schemas lxappearance polkit dbus brightnessctl
